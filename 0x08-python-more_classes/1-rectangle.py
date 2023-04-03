b4e34ae0945c7734362637b8ef18c330e2d9af0a
Sunny670 <sunny.thanki13@gmail.com>
2023-04-03 08:41:42 -0700

    bugs fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,42 +1,47 @@
-#!/usr/bin/python3
+#!/usr/bin/pytho3
 # 1-rectangle.py
 """Defines a Rectangle class."""
 
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-
-        """Initialize a new Rectangle.
-        Args:
-            width (int):  Width of  new rectangle.
-            height (int): Height of  new rectangle.
-        """
-
-    self.width = width
-    self.height = height
+        """ Initialize the width and height of the rectangle
+        with default values of 0"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
-        return self.__width
+        """ Getter method for the width property"""
+    return self.__width
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        """ Setter method for the width property
+            Check that the input value is an integer
+        """
+    if not isinstance(value, int):
+        raise TypeError("width must be an integer")
+    """ Check that the input value is greater than or equal to 0"""
+    if value < 0:
+        raise ValueError("width must be >= 0")
+    """ Set the value of the width attribute to the input value"""
+    self.__width = value
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
-        return self.__height
+        """ Getter method for the height property"""
+    return self.__height
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        """ Setter method for the height property
+            Check that the input value is an integer
+        """
+    if not isinstance(value, int):
+        raise TypeError("height must be an integer")
+    """ Check that the input value is greater than or equal to 0"""
+    if value < 0:
+        raise ValueError("height must be >= 0")
+    """ Set the value of the height attribute to the input value"""
+    self.__height = value
