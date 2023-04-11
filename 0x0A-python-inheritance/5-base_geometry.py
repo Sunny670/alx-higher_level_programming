31659684a98a29a263b24fb82b88325964470390
Sunny670 <sunny.thanki13@gmail.com>
2023-04-11 21:28:26 +0300

    tasks 5 & 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+"""Contains a class BaseGeometry which is empty."""
+
+
+class BaseGeometry:
+    """Represent a base geometry."""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""Hasa class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Class that's a public attribute area"""
+
+    def area(self):
+        """exception raised when called"""
+        raise Exception("area() is not implemented")
