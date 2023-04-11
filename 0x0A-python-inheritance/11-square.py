2b03f6b405b52b41019f17b40b47647987ca8eef
Sunny670 <sunny.thanki13@gmail.com>
2023-04-11 12:47:10 -0700

    bugs fixed

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,6 +4,7 @@
 Rectangle = __import__('9-rectangle').Rectangle
 """Function is imported from task 9."""
 
+
 class Square(Rectangle):
     """Function that represents a square."""
 
@@ -16,6 +17,7 @@ class Square(Rectangle):
     def area(self):
         """area of the square."""
         return self.__size ** 2
+
     def __str__(self):
         """string representation of square."""
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
