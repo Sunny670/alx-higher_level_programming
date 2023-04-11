387dd49b4c068caacfc2302f162a1cfe6a63c7bd
Sunny670 <sunny.thanki13@gmail.com>
2023-04-11 21:15:22 +0300

    tasks2 & 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""Contains a class and inherited class-checking function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """The function returns true if the object is an instance or inherited
+       from a_class, else False.    
+    """
+    return (isinstance(obj, a_class))
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 4-inherits_from.py
+"""Has an inherited_from class-checking function."""
+
+
+def inherits_from(obj, a_class):
+    """Returns true if obj is subclass to a_class,else False."""
+    
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
