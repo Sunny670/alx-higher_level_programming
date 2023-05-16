258157fc5033d3d8bffd61d4d52807464f03e2d1
Sunny670 <sunny.thanki13@gmail.com>
2023-05-16 21:41:47 +0300

    tasks 1-9

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Prints full description of table first_table in my MySQL server.
+SHOW CREATE TABLE `first_table`;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- List all rows of table first_table in my MySQL server.
+SELECT * FROM `first_table`;
